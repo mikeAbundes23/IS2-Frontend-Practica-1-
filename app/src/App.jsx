93b58ca008d41373b8d973be5db71a241ee59ae7
mikeAbundes23 <irvingmiguel.abundes@ciencias.unam.mx>
2025-08-24 15:39:11 -0600

    chore(frontend): configuración inicial con React y Vite

---
+++ app/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="flex items-center justify-center min-h-screen bg-red-500">
+        <h1 className="text-3xl font-bold">Hello World</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ app/src/context/AuthContext.jsx
@@ -0,0 +1,142 @@
+import { createContext, useState, useEffect } from "react";
+import { jwtDecode } from "jwt-decode";
+import { useNavigate } from "react-router-dom";
+
+const AuthContext = createContext();
+
+export default AuthContext;
+
+export const AuthProvider = ({ children }) => {
+  let [authTokens, setAuthTokens] = useState(() =>
+    localStorage.getItem("authTokens")
+      ? JSON.parse(localStorage.getItem("authTokens"))
+      : null
+  );
+  let [user, setUser] = useState(() =>
+    localStorage.getItem("authTokens")
+      ? jwtDecode(localStorage.getItem("authTokens"))
+      : null
+  );
+
+  let [profile, setProfile] = useState(() =>
+    localStorage.getItem("profile")
+      ? JSON.parse(localStorage.getItem("profile"))
+      : null
+  );
+
+  let [loading, setLoading] = useState(true);
+
+  const navigate = useNavigate();
+
+  let loginUser = async (e) => {
+    e.preventDefault();
+    let response = await fetch("http://127.0.0.1:8000/api/token/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        email: e.target.email.value,
+        password: e.target.password.value,
+      }),
+    });
+
+    let data = await response.json();
+
+    if (response.status === 200) {
+      setAuthTokens(data);
+      setUser(jwtDecode(data.access));
+      localStorage.setItem("authTokens", JSON.stringify(data));
+
+      // 👇 pedir el perfil del usuario
+      let profileRes = await fetch("http://127.0.0.1:8000/api/user/me/", {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + data.access,
+        },
+      });
+
+      if (profileRes.status === 200) {
+        let profileData = await profileRes.json();
+        setProfile(profileData);
+        localStorage.setItem("profile", JSON.stringify(profileData));
+      }
+
+      navigate("/");
+    } else {
+      console.log("Something went wrong");
+    }
+  };
+
+  let logoutUser = () => {
+    setAuthTokens(null);
+    setUser(null);
+    setProfile(null);
+    localStorage.removeItem("authTokens");
+    localStorage.removeItem("profile");
+    navigate("/");
+  };
+
+  let updateToken = async () => {
+    console.log("Updating token...");
+    let response = await fetch("http://127.0.0.1:8000/api/token/refresh/", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({
+        refresh: authTokens?.refresh,
+      }),
+    });
+
+    let data = await response.json();
+
+    if (response.status === 200) {
+      setAuthTokens(data);
+      setUser(jwtDecode(data.access));
+      localStorage.setItem("authTokens", JSON.stringify(data));
+    } else {
+      logoutUser();
+    }
+
+    if (loading) {
+      setLoading(false);
+    }
+  };
+
+  let contextData = {
+    user,
+    profile,
+    authTokens,
+    loginUser,
+    logoutUser,
+  };
+
+  useEffect(() => {
+    if (!authTokens) {
+      setLoading(false);
+      return;
+    }
+
+    if (loading) {
+      updateToken();
+    }
+
+    let fourMinutes = 1000 * 60 * 25;
+    let interval = setInterval(() => {
+      if (authTokens) {
+        //setLoading(true);
+        updateToken();
+      }
+    }, fourMinutes);
+
+    return () => clearInterval(interval);
+  }, [authTokens, loading]);
+
+  return (
+    <AuthContext.Provider value={contextData}>
+      {loading ? null : children}
+    </AuthContext.Provider>
+  );
+};
